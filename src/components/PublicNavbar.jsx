2767ea5de0fec5fae576d9bf392710041b374619
abideenpj-bit <abideenpj@gmail.com>
2026-06-03 15:46:45 +0500

    Update PublicNavbar.jsx

--- src/components/PublicNavbar.jsx
+++ src/components/PublicNavbar.jsx
@@ -76,7 +76,7 @@ const PublicNavbar = () => {
                   </NavLink>
 
                   <a
-                    href="https://stomuniver.ru/0205-9592/index"
+                    href=""
                     target="_blank"
                     rel="noopener noreferrer"
                     className="block px-5 py-2.5 text-sm text-slate-600 hover:bg-indigo-50 hover:text-indigo-700"
