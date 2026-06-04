7d35a9cf0accf9c5b3c986fc06090a695390a259
abideenpj-bit <abideenpj@gmail.com>
2026-06-04 10:28:58 +0500

    Update OldPublished.jsx

--- src/pages/OldPublished.jsx
+++ src/pages/OldPublished.jsx
@@ -2,7 +2,7 @@
 const OldPublished = () => {
   return (
     <div>
-      <h1 classname="text-center">Old Published Articles</h1>
+      <h1 classname="text-center"> Old Published Articles</h1>
     </div>
   );
 };
