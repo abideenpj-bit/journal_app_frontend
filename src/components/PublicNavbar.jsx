e413839155e2b82aebb251ef83ee5a2a74dfc459
abideenpj-bit <abideenpj@gmail.com>
2026-06-12 10:37:11 +0500

    Update PublicNavbar.jsx

--- src/components/PublicNavbar.jsx
+++ src/components/PublicNavbar.jsx
@@ -76,7 +76,7 @@ const PublicNavbar = () => {
                   </NavLink>
 
 <NavLink to="/old-published" className="block px-5 py-2.5 text-sm text-slate-600 hover:bg-indigo-50 hover:text-indigo-700" >
-  Old Publish
+  Old Published
   
 </NavLink>
   
